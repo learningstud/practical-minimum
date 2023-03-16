3869b6938adf32c33e06ca6b3616317eb159db3a
learningstud <learningstud@gmail.com>
2023-03-16 10:34:37 +0800

    Genesis

---
+++ lakefile.lean
@@ -0,0 +1,10 @@
+import Lake
+open Lake DSL
+
+package «practical-minimum»
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
+
+@[default_target]
+lean_lib PracticalMinimum
