47dadf0cf49e9c86fdc81f473feca89a5e1843eb
learningstud <learningstud@gmail.com>
2023-05-04 10:35:23 +0800

    Generalize `Binop.div` to `FallibleOp`

--- FP/Monad/Arith.lean
+++ FP/Monad/Arith.lean
@@ -4,26 +4,35 @@ inductive Expr.{u} (Op: Type u)
   | const (value: Int)
   | binop (op: Op) (left right: Expr Op)
 
-inductive Binop | add | sub | mul | div
+inductive Binop.{u} (FallibleOp: Type u)
+  | add | sub | mul
+  | fallible (op: FallibleOp)
 
-def applyDivOption (x y: Int): Option Int :=
-  if y = 0 then none else x / y
+inductive FallibleOp
+  | div
 
-def applyDivExcept (x y: Int): Except String Int :=
-  if y = 0 then .error s!"Divide {x} by zero" else .ok (x / y)
+def applyFallibleOption (x y: Int): FallibleOp → Option Int
+  | .div => if y = 0 then none else x / y
 
-def applyOp {m} [Monad m] (applyDiv: Int → Int → m Int) (x y: Int): Binop → m Int
+def applyFallibleExcept (x y: Int): FallibleOp → Except String Int
+  | .div => if y = 0 then .error s!"Divide {x} by zero" else .ok (x / y)
+
+def applyOp {m} [Monad m] (applyFallible: Int → Int → FallibleOp → m Int) (x y: Int)
+  : Binop FallibleOp → m Int
   | .add => pure (x + y)
   | .sub => pure (x - y)
   | .mul => pure (x * y)
-  | .div => applyDiv x y
+  | .fallible op => applyFallible x y op
 
-def evaluateM {m} [Monad m] (applyDiv: Int → Int → m Int): Expr Binop → m Int
+def evaluateM {m} [Monad m] (applyFallible: Int → Int → FallibleOp → m Int)
+  : Expr (Binop FallibleOp) → m Int
   | .const value => pure value
   | .binop op left right =>
-    evaluateM applyDiv left >>= fun x =>
-    evaluateM applyDiv right >>= fun y =>
-    applyOp applyDiv x y op
+    evaluateM applyFallible left >>= fun x =>
+    evaluateM applyFallible right >>= fun y =>
+    applyOp applyFallible x y op
 
-def evaluateOption: Expr Binop → Option Int := evaluateM applyDivOption
-def evaluateExcept: Expr Binop → Except String Int := evaluateM applyDivExcept
+def evaluateOption: Expr (Binop FallibleOp) → Option Int :=
+  evaluateM applyFallibleOption
+def evaluateExcept: Expr (Binop FallibleOp) → Except String Int :=
+  evaluateM applyFallibleExcept
